ba5f21b09b5e3eaacd58136bc6ee6953796836ba
Christian <christian.parodi14@gmail.com>
2020-03-14 10:53:54 -0300

    ActionBar
    Se agregan botones e imágenes al action bar en MenuActivity

--- app/src/main/java/com/test/changasuy/MenuActivity.java
+++ app/src/main/java/com/test/changasuy/MenuActivity.java
@@ -1,11 +1,15 @@
 package com.test.changasuy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -66,10 +70,32 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuprincipal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                Toast.makeText(getApplicationContext(),"Search",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.configuracion:
+                Toast.makeText(getApplicationContext(),"Configuracion",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu:
+                Toast.makeText(getApplicationContext(),"Menu",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.compartir:
+                Toast.makeText(getApplicationContext(),"Compartir",Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
