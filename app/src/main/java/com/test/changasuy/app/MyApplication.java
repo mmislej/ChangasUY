4b91c908b26abcd9c2174d95b422857cfbeda07f
mmislej <61766575+mmislej@users.noreply.github.com>
2020-03-16 17:42:09 -0300

    Realm Config
    Configuracion de la base

--- app/src/main/java/com/test/changasuy/app/MyApplication.java
+++ app/src/main/java/com/test/changasuy/app/MyApplication.java
@@ -9,8 +9,10 @@ import com.test.changasuy.models.Trabajo;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import io.realm.RealmObject;
 import io.realm.RealmResults;
+
 
 public class MyApplication extends Application {
 
@@ -21,11 +23,22 @@ public class MyApplication extends Application {
     @SuppressLint("MissingSuperCall")
     @Override
     public void onCreate() {
-        
+        setUpRealmConfig();
         Realm realm = Realm.getDefaultInstance();
         TrabajoID = getIdByTable(realm, Trabajo.class);
         PostuladoID = getIdByTable(realm, Postulado.class);
         realm.close();
+
+    }
+
+    private void setUpRealmConfig(){
+        Realm.init(getApplicationContext());
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .name(Realm.DEFAULT_REALM_NAME)                         // Tuve que modificar esto para que compile
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(config);
+
 
     }
 
